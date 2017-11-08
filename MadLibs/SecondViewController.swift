0cb7bca6b4637b4a225d2d9f47d427e0c0e85945
Elva Wang <elva316@gmail.com>
2017-11-08 12:34:54 -0800

    first commit

---
+++ MadLibs/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MadLibs
+//
+//  Created by elva wang on 11/8/17.
+//  Copyright © 2017 elva wang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    @IBOutlet weak var text3: UITextField!
+    @IBOutlet weak var text4: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+}
--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    var output: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        outputLabel.text = "..."
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func unwindfunc (segue:UIStoryboardSegue) {
+        print("hello")
+        if let controller = segue.source as? SecondViewController {
+        
+                let t1 = controller.text1.text!
+                let t2 = controller.text2.text!
+                let t3 = controller.text3.text!
+                let t4 = controller.text4.text!
+
+            let str = "We are having a perfectly \(t1) time now.  Later we will \(t2) and \(t3) in the \(t4)."
+            outputLabel.text = str
     }
-
-
+    }
+    
 }
 
